f0d1083e87c0f10a642c67cbe0922679ce034e9d
toldii1 <100698275+toldii@users.noreply.github.com>
2022-05-24 13:07:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+(function () {
+ex1_button.addEventListener("click", function (){
+  ex1_content.innerHTML = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+  
+})
+  ex2_tekst.addEventListener("sprawdz tekst", function(){
+    ex2_
+  })
+})();
